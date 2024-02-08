9627a3b17bdbd868ec3897bac2286715f25e9e2f
totoro2012adri <94864587+totoro2012adri@users.noreply.github.com>
2024-02-08 11:01:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "eu-central-1"
-  #secret_key = "pD5OqEtFA05j7Aj96jGzGqYf7/etpLZhgkhybLDZ"
-  #access_key = "AKIA6GBMFXARZE3NJBIA"
+  secret_key = "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"
+  access_key = "AKIA6GBMFXARZE3NJBIA"
 }
 
 resource "aws_instance" "Prueba1_ec2" {
